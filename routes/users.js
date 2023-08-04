89de66001d3c6e328eabcd7335688032a6bdfefb
TKD10 <103336180+TKD10@users.noreply.github.com>
2023-08-04 14:28:11 -0700

    added users to routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+//const { eventNames } = require('.');
 const router = express.Router();
 const archive_url = 'http://localhost:8080/archive';
 
@@ -13,9 +14,23 @@ const ReadArchive = async (url) => {
 //below is simple api
 router.get('/get_user_info', async (req, res, next) => {
     const data = await ReadArchive(archive_url);
-    if (data != null){
+    if (data != NULL || data != undefined){
         //json parsing
         return res.status(200).json(data); //just returns the entire data
+    }
+    else {
+        return res.status(401).json({"message":"Error!"}); //simple error checking
+    }
+});
+
+router.get('/get_first_names', async (req, res, next) => {
+    const data = await ReadArchive(archive_url);
+    if (data != NULL || data != undefined){
+        console.log(data.persons.length);
+        for (i = 0; i < data.persons.length; i++){
+            names.push(data.persons[i].firstname);
+        }
+        return res.status(200).json(names)
     }
     else {
         return res.status(401).json({"message":"Error!"}); //simple error checking
